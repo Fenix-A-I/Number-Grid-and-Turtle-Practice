e589bb3e4e9ded6072206f0e6b6c5fede162840e
A.I. <33167026+Fenix-A-I@users.noreply.github.com>
2025-06-18 01:20:41 -0700

    Completed function that draws radial lines in each of four quadrants

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -32,7 +32,7 @@ def draw_line(t, x, y, angle, length):
 #draw_line(t, -100, 50, 90, 120)
 #draw_line(t, 60, -80, 180, 60)
 
-def draw_radial_lines(t, x, y, length, num_lines):
+def draw_rad_lines(t, x, y, length, num_lines):
     """
     Draws multiple lines (radials) originating from a common point, evenly spaced around a full circle.
 
@@ -54,6 +54,35 @@ def draw_radial_lines(t, x, y, length, num_lines):
     for i in range(num_lines):
         draw_line(t, x, y, i * angle, length)
         
-#draw_radial_lines(t, 0, 0, 50, 6)
-#draw_radial_lines(t, 100, 100, 30, 12)
-#draw_radial_lines(t, -100, 50, 70, 5)
+#draw_rad_lines(t, 0, 0, 50, 6)
+#draw_rad_lines(t, 100, 100, 30, 12)
+#draw_rad_lines(t, -100, 50, 70, 5)
+
+def draw_rad_in_quad(t, length, num_lines):
+    """
+    Draws sets of radial lines in each of the four quadrants, separated from the origin.
+
+    For each quadrant, this function positions the center of a radial pattern at a distance of 2 * length
+    from the origin along both axes (positive and negative), ensuring the radials in each quadrant do not overlap.
+    It then draws a set of evenly spaced radial lines in each quadrant by calling draw_radial_lines with
+    the appropriate (x, y) coordinates.
+
+    Args:
+        t (Turtle): The turtle object used for drawing.
+        length (int or float): The length of each radial line.
+        num_lines (int): The number of radial lines to draw in each quadrant.
+
+    Returns:
+        None
+    """
+    x = 2 * length
+    y = 2 * length
+    draw_rad_lines(t, x, y, length, num_lines)
+    draw_rad_lines(t, -x, y, length, num_lines)
+    draw_rad_lines(t, -x, -y, length, num_lines)
+    draw_rad_lines(t, x, -y, length, num_lines)
+
+#draw_rad_in_quad(t, 50, 9)
+#draw_rad_in_quad(t, 30, 6)
+#draw_rad_in_quad(t, 80, 12)
+
